f90d9e57e10b0e2192a22396a11e2e2fda21adbb
ThatTechedGuy <vgupta463@gmail.com>
2019-07-18 21:47:10 +0530

    NOTES 2.0 FEATURE WITH FIREBASE AND FIRESTORE.NEW DATA FLOW SYSTEM.

--- app/src/main/java/com/example/icasapp/Notes/NotesFragment.java
+++ app/src/main/java/com/example/icasapp/Notes/NotesFragment.java
@@ -1,52 +1,41 @@
 package com.example.icasapp.Notes;
-
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
-
-
 import com.example.icasapp.R;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnPausedListener;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import java.util.HashMap;
+import java.util.Map;
 
 import static android.app.Activity.RESULT_OK;
 
-/**
-
- * A simple {@link Fragment} subclass.
-
- */
-
 public class NotesFragment extends Fragment {
 
-    EditText inputFileName;
     Button uploadButton;
     Button viewButton;
-
-    String NAME;
-    String URL;
-
-
-    DatabaseReference databaseReference;
-    StorageReference storageReference;
-
     View notesView;
+    UploadTask uploadTask;
+    StorageReference storageRef;
+    String SEMESTER , SUBJECT , SESSIONAL, SUBJECT_ABBREVIATION , FILE_NAME_BY_USER;
+    FirebaseFirestore db ;
 
     public NotesFragment() {
         // Required empty public constructor
@@ -55,102 +44,131 @@ public class NotesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        NAME = URL = "";
-
+        // Inflate the layout for this fragment
         notesView = inflater.inflate(R.layout.fragment_notes, container, false);
 
-
-        inputFileName = notesView.findViewById(R.id.inputFileName);
         uploadButton = notesView.findViewById(R.id.uploadButton);
         viewButton = notesView.findViewById(R.id.viewButton);
 
-        storageReference = FirebaseStorage.getInstance().getReference();
-        databaseReference = FirebaseDatabase.getInstance().getReference("uploads");
+        storageRef = FirebaseStorage.getInstance().getReference();
 
+
+        //FUNCTIONALITY WHEN UPLOAD BUTTON CLICKED.
         uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                selectDocument();
+                showFileChooser();
             }
         });
-
         viewButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), ViewNotesActivity.class);
-                startActivity(intent);
+
             }
         });
-
-        // Inflate the layout for this fragment
         return notesView;
     }
 
-
-    //SELECT DOCUMENT FROM DEVIC FROM LOCAL DIRECTORY.
-    private void selectDocument() {
-        Intent intent = new Intent();
-        //FOR PDF FILES
-        intent.setType("application/pdf");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "SELECT PDF FILE"), 1);
+    //ON UPLOAD BUTTON CLICK , CHOOSE FILE
+    private void showFileChooser() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");      //all files
+        //intent.setType("file/pdf");   //PDF file only
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        try {
+            startActivityForResult(Intent.createChooser(intent, "Select a File to Upload"), 1);
+        } catch (android.content.ActivityNotFoundException ex) {
+            // Potentially direct the user to the Market with a Dialog
+            Toast.makeText(getContext(), "Please install a File Manager.", Toast.LENGTH_SHORT).show();
+        }
     }
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
+        //ACTIONS TO DO IF THE FILE HAS BEEN SUCCESSFULLY SELECTED.
         if (requestCode == 1 && resultCode == RESULT_OK
                 && data != null && data.getData() != null) {
-            uploadPDFFile(data.getData());
+            startActivity(new Intent(getContext() , NotesForm.class));
         }
     }
 
-    private void uploadPDFFile(Uri data) {
-        //MANAGING PROGRESS AND STORAGE OF UPLOADED FILE INTO CLOUD
-
-        StorageReference reference = storageReference.child("uploads/" + System.currentTimeMillis() + ".pdf");
-        NAME = inputFileName.getText().toString();
-
-        if (NAME.equals("")) {
-            Toast.makeText(getContext(), "GIVE A FILE NAME TO THE PDF", Toast.LENGTH_LONG).show();
-            inputFileName.requestFocus();
-        } else {
-
-            final ProgressDialog progressDialog = new ProgressDialog(this.getContext());
-            progressDialog.setTitle("UPLOADING.....");
-            progressDialog.show();
-
-            reference.putFile(data)
-                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            Task<Uri> uri = taskSnapshot.getStorage().getDownloadUrl();
-                            while (!uri.isComplete()) ;
-                            Uri url = uri.getResult(); //URL OF FILE STORED IN DATABASE
-                            URL = url.toString();
-
-                            DocumentHelper documentHelper = new DocumentHelper(NAME, URL);
-                            databaseReference.child(databaseReference.push().getKey()).setValue(documentHelper);
-
-                            Toast.makeText(getContext(), "UPLOADED SUCCESSFULLY", Toast.LENGTH_LONG).show();
-                            progressDialog.dismiss();
-
-                        }
-                    })
-                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-
-                            double progress = (100 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
-                            progressDialog.setMessage("UPLOADED :" + (int) progress + "%");
-                        }
-                    })
-            ;
-
-        }
 
 
-    }
+   //INSIDE THE INTENT AFTER CLICK ON UPLOAD BUTTON, HANDLING FILE TO BE UPLOADED AND PATH.
+
+
+    //FIREBASE FILE UPLOAD FUNCTIONALITY.
+    private void uploadFile(Uri DATA) {
+        Log.i("msg" , "REACHED UPLOAD FILE FUNCTION.");
+
+        String filename =  new FileHelper().getFileName(DATA);
+        Log.i("msg" , "INITIAL FILE NAME:"+ filename);
+        if(FILE_NAME_BY_USER == null || FILE_NAME_BY_USER.equals("") || FILE_NAME_BY_USER.equals(" "))
+            FILE_NAME_BY_USER = filename;
+
+        final StorageReference ref = storageRef.child("NOTES/"+ DATA.toString());
+        uploadTask = ref.putFile(DATA);
+
+        Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                if (!task.isSuccessful()) {
+                    throw task.getException();
+                }
+                Log.i("msg", "successful");
+                // Continue with the task to get the download URL
+                return ref.getDownloadUrl();
+            }
+        });
+        // Listen for state changes, errors, and completion of the upload
+        uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                Log.i("msg" ,"Upload is " + progress + "% done");
+            }
+        }).addOnPausedListener(new OnPausedListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onPaused(UploadTask.TaskSnapshot taskSnapshot) {
+                Log.i("msg" ,"Upload is paused");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+                Log.i("msg" ,"Upload has failed");
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // Handle successful uploads on complete
+                // ...
+                Log.i("msg" ,"Upload has been successful.");
+            }
+        });
+
+
+        urlTask.addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if (task.isSuccessful()) {
+                    Uri downloadUri = task.getResult();
+                    Log.i("msg" , downloadUri.toString());
+                    Log.i("msg" , "UPLOAD COMPLETED.");
+                } else {
+                    // Handle failures
+                    // ...
+                    Log.i("msg" , "UPLOAD FAILURE.");
+
+                }
+            }
+        });
+    }//END OF uploadFile() FUNCTION.
+
 }
+
+
+
+
+
+
