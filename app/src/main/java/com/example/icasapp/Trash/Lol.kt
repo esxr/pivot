e2435636693a67d9f2b50ad3b4e4d7fea128426c
Yonarp <pranoy0211@gmail.com>
2019-07-28 23:11:51 +0530

    added some paddings and margins and made the text bold

--- app/src/main/java/com/example/icasapp/Trash/Lol.kt
+++ app/src/main/java/com/example/icasapp/Trash/Lol.kt
@@ -1,10 +1,1 @@
 package com.example.icasapp.Trash
-/*
-class BankAccount {
-  /*  var name: String = "Lolwa";
-    var balance: Int = 10;
-
-    fun getBalance(): Int {
-        return balance
-    }*/
-}
